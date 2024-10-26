7102e684e04f22f56b29af143bd6d20b07b3ff86
Zi Hao Wong <111846088+Wong86-cloud@users.noreply.github.com>
2024-10-26 09:30:31 +0800

    update the database content

--- db_content/kpop_e-commerce.sql
+++ db_content/kpop_e-commerce.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 22, 2024 at 02:40 PM
+-- Generation Time: Oct 26, 2024 at 03:28 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,6 +20,32 @@ SET time_zone = "+00:00";
 --
 -- Database: `kpop_e-commerce`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admins`
+--
+
+CREATE TABLE `admins` (
+  `admin_id` int(11) NOT NULL,
+  `unique_id` int(200) NOT NULL,
+  `fname` varchar(255) NOT NULL,
+  `lname` varchar(255) NOT NULL,
+  `gender` varchar(10) DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
+  `admin_work_code` varchar(50) NOT NULL,
+  `img` varchar(400) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL DEFAULT 'Offline Now'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admins`
+--
+
+INSERT INTO `admins` (`admin_id`, `unique_id`, `fname`, `lname`, `gender`, `email`, `admin_work_code`, `img`, `password`, `status`) VALUES
+(1, 759044220, 'Zi Hao', '(Admin)', 'male', 'zihaowong@admin.com.my', '2818', 'admin.png', '$2y$10$SATLIdumA5a.xd5J119dEO9iwd8WYxLrgsrNYrgYZvnYyx3Ec1CQy', 'Offline now');
 
 -- --------------------------------------------------------
 
@@ -70,6 +96,33 @@ INSERT INTO `cart` (`cart_id`, `unique_id`, `product_id`, `quantity`, `product_n
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `feedback`
+--
+
+CREATE TABLE `feedback` (
+  `feedback_id` int(6) UNSIGNED NOT NULL,
+  `unique_id` int(200) NOT NULL,
+  `question_1` varchar(255) NOT NULL,
+  `question_2` varchar(255) NOT NULL,
+  `question_3` text NOT NULL,
+  `question_4` varchar(255) NOT NULL,
+  `question_5` varchar(255) NOT NULL,
+  `question_6` varchar(255) NOT NULL,
+  `question_7` text NOT NULL,
+  `question_8` varchar(255) NOT NULL,
+  `submitted_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `feedback`
+--
+
+INSERT INTO `feedback` (`feedback_id`, `unique_id`, `question_1`, `question_2`, `question_3`, `question_4`, `question_5`, `question_6`, `question_7`, `question_8`, `submitted_at`) VALUES
+(41, 1517014417, '4', '4', 'Albums, Photocards', 'Easy', 'PayPal', 'No, everything was smooth', 'Discussion forums', '4', '2024-10-23 10:31:20');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `friends`
 --
 
@@ -85,7 +138,7 @@ CREATE TABLE `friends` (
 
 INSERT INTO `friends` (`friend_id`, `unique_id`, `friend_unique_id`) VALUES
 (7, 1655463620, 1517014417),
-(9, 1517014417, 1655463620);
+(11, 1517014417, 1655463620);
 
 -- --------------------------------------------------------
 
@@ -117,7 +170,10 @@ INSERT INTO `messages` (`msg_id`, `incoming_msg_id`, `outgoing_msg_id`, `msg`, `
 (30, 1517014417, 1655463620, 'Yahoo', ''),
 (31, 1517014417, 1655463620, 'how\\\'s your day', ''),
 (47, 1655463620, 1517014417, 'great', NULL),
-(48, 1655463620, 1517014417, 'how about u', '');
+(99, 1655463620, 1517014417, '', 'assets/images/uploads/liampayne.jpg'),
+(100, 1655463620, 1517014417, 'Liam Payne passed away So sad', ''),
+(101, 1655463620, 1517014417, '😭😭😭', ''),
+(102, 1655463620, 1517014417, 'halo?', '');
 
 -- --------------------------------------------------------
 
@@ -144,7 +200,7 @@ CREATE TABLE `orders` (
 --
 
 INSERT INTO `orders` (`order_id`, `order_cost`, `order_status`, `unique_id`, `country_code`, `handphone`, `address`, `postcode`, `city`, `order_date`, `shipping_id`) VALUES
-(24, 22.51, 'Pending', 1655463620, '+81', '0335467689', 'Shibuya Ward, Jinnan 2-1-2-3 Shibuya Heights Room 101', '〒150-0041', 'Shibuya-ku, Tokyo-to', '2024-10-13 23:08:29', 3),
+(24, 22.51, 'Received', 1655463620, '+81', '0335467689', 'Shibuya Ward, Jinnan 2-1-2-3 Shibuya Heights Room 101', '〒150-0041', 'Shibuya-ku, Tokyo-to', '2024-10-13 23:08:29', 3),
 (27, 34.68, 'Pending', 1655463620, '+81', '0335467689', 'Shibuya Ward, Jinnan 2-1-2-3 Shibuya Heights Room 101', '〒150-0041', 'Shibuya-ku, Tokyo-to', '2024-10-14 12:56:31', 3),
 (31, 116.91, 'Pending', 1655463620, '+81', '0335467689', 'Shibuya Ward, Jinnan 2-1-2-3 Shibuya Heights Room 101', '〒150-0041', 'Shibuya-ku, Tokyo-to', '2024-10-18 12:03:08', 2),
 (32, 70.04, 'Pending', 1655463620, '+81', '0335467689', 'Shibuya Ward, Jinnan 2-1-2-3 Shibuya Heights Room 101', '〒150-0041', 'Shibuya-ku, Tokyo-to', '2024-10-22 02:01:43', 2);
@@ -182,6 +238,29 @@ INSERT INTO `order_details` (`item_id`, `unique_id`, `order_id`, `order_date`, `
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `order_issues`
+--
+
+CREATE TABLE `order_issues` (
+  `issue_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `status` enum('pending','resolved') NOT NULL DEFAULT 'pending',
+  `issue_description` text NOT NULL,
+  `issue_image` varchar(400) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `order_issues`
+--
+
+INSERT INTO `order_issues` (`issue_id`, `order_id`, `user_id`, `status`, `issue_description`, `issue_image`, `created_at`) VALUES
+(1, 31, 1655463620, 'resolved', 'Where is my passport cover?', 'missing.png\r\n', '2024-10-26 00:17:35');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `personal_photos`
 --
 
@@ -201,7 +280,8 @@ INSERT INTO `personal_photos` (`photo_id`, `unique_id`, `photo_url`, `upload_dat
 (13, 1655463620, 'assets/images/uploads/anna_pic2.png', '2024-10-19 14:38:58'),
 (15, 1655463620, 'assets/images/uploads/anna_pic4.jpg', '2024-10-19 14:40:00'),
 (16, 1655463620, 'assets/images/uploads/anna_pic3.png', '2024-10-19 14:41:45'),
-(19, 1655463620, 'assets/images/uploads/anna_pic5.png', '2024-10-21 13:40:51');
+(19, 1655463620, 'assets/images/uploads/anna_pic5.png', '2024-10-21 13:40:51'),
+(20, 1517014417, 'assets/images/uploads/ella2.jpg', '2024-10-25 12:03:04');
 
 -- --------------------------------------------------------
 
@@ -226,9 +306,10 @@ CREATE TABLE `posts` (
 --
 
 INSERT INTO `posts` (`room_id`, `post_id`, `unique_id`, `post`, `post_image`, `post_comments`, `post_likes`, `post_date`, `got_image`) VALUES
-(1, 1, 1655463620, 'ROSÉ finally released her new song! Luv it~', 'assets/images/uploads/APT..png', 0, 0, '2024-10-22 02:41:08', 1),
+(1, 1, 1655463620, 'ROSÉ finally released her new song! Luv it~', 'assets/images/uploads/APT..png', 0, 1, '2024-10-25 04:02:44', 1),
 (2, 4, 1655463620, 'OMG😭! All of them looks so gorgeous✨!', 'assets/images/uploads/aespa.jpeg', 2, 2, '2024-10-22 05:01:56', 1),
-(1, 6, 1517014417, 'Congrats😍 to our unnie who takes 1st win for \"Mantra\" on M Countdown.', 'assets/images/uploads/jennie-mcountdown.jpg', 1, 2, '2024-10-22 05:05:48', 1);
+(1, 6, 1517014417, 'Congrats😍 to our unnie who takes 1st win for \"Mantra\" on M Countdown.', 'assets/images/uploads/jennie-mcountdown.jpg', 1, 2, '2024-10-22 05:05:48', 1),
+(NULL, 9, 1517014417, 'sjdjhshdhsdjhjhf🙈', '', 1, 1, '2024-10-25 04:02:20', 0);
 
 -- --------------------------------------------------------
 
@@ -251,7 +332,8 @@ CREATE TABLE `post_comments` (
 INSERT INTO `post_comments` (`comment_id`, `post_id`, `unique_id`, `comment`, `comment_date`) VALUES
 (24, 4, 1655463620, 'yessss😘', '2024-10-20 10:59:34'),
 (27, 4, 1517014417, 'ATE💋', '2024-10-21 18:42:08'),
-(32, 6, 1655463620, 'ATE!!', '2024-10-22 05:05:48');
+(32, 6, 1655463620, 'ATE!!', '2024-10-22 05:05:48'),
+(33, 9, 1517014417, 'yay', '2024-10-25 04:02:11');
 
 --
 -- Triggers `post_comments`
@@ -282,10 +364,12 @@ CREATE TABLE `post_likes` (
 --
 
 INSERT INTO `post_likes` (`like_id`, `post_id`, `unique_id`) VALUES
+(35, 1, 1517014417),
 (31, 4, 1517014417),
 (29, 4, 1655463620),
 (32, 6, 1517014417),
-(33, 6, 1655463620);
+(33, 6, 1655463620),
+(34, 9, 1517014417);
 
 -- --------------------------------------------------------
 
@@ -308,21 +392,21 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`product_id`, `product_category`, `celebrity`, `product_image`, `product_name`, `product_price`, `product_description`) VALUES
-(1, 'Merchandise', 'BTS', 'merch1.png', 'Passport Cover', 15.58, ''),
+(1, 'Merchandise', 'BTS', 'merch1.png', 'Passport Cover', 15.58, 'Stylish and durable, this BTS Passport Cover keeps your travel documents safe while showcasing your fandom.'),
 (2, 'Merchandise', 'BTS', 'merch2.png', 'Travel Pouch (Blue)', 23.78, 'Compact BTS travel pouch, perfect for organizing your accessories and essentials on the go.'),
-(3, 'Merchandise', 'BTS', 'merch3.png', 'S/S T-Shirt (Ivory)', 40.18, ''),
-(4, 'Album', 'BTS', 'album1.png', 'V (BTS) \'Layover\' LP', 42.64, ''),
-(5, 'Album', 'BTS', 'album2.png', 'Jimin (BTS) \'FACE\' LP', 42.64, ''),
-(6, 'Album', 'BTS', 'album3.png', 'Jimin (BTS) \'MUSE\' (Set)', 32.47, ''),
-(7, 'Photobook', 'BTS', 'photobook1.png', 'Jin ‘Sea of JIN island’', 42.64, ''),
-(8, 'Photobook', 'BTS', 'photobook2.png', 'BTS \'WE\' SET', 42.64, ''),
-(9, 'Photobook', 'BTS', 'photobook3.png', 'SUGA ‘Wholly or Whole me’', 27.06, ''),
-(10, 'Photocard', 'BTS', 'photocard1.png', 'BTS V layover POB PC', 7.00, ''),
-(11, 'Photocard', 'BTS', 'photocard2.jpg', 'Photocard Set (BTS)', 15.58, ''),
-(12, 'Photocard', 'BTS', 'photocard3.png', 'BTS V FRIENDS Photocards 2024', 20.68, ''),
-(13, 'Album', 'BTS', 'album_bts.png', 'RM (BTS) \'Right Place, Wrong Person\' (Set)', 48.70, ''),
-(15, 'Merchandise', 'BLACKPINK', 'merch4.png', 'OFFICIAL LIGHT STICK ver.2', 31.98, ''),
-(24, 'Photocard', 'BLACKPINK', 'photocard4.png', '[ME] JISOO PHOTO CARD DECO KIT', 9.84, '');
+(3, 'Merchandise', 'BTS', 'merch3.png', 'S/S T-Shirt (Ivory)', 40.18, 'Comfortable and stylish, this ivory short sleeve T-shirt features a trendy BTS graphic for casual wear.'),
+(4, 'Album', 'BTS', 'album1.png', 'V (BTS) \'Layover\' LP', 42.64, 'Enjoy V\'s soothing melodies with this \'Layover\' LP, a must-have for any music lover.'),
+(5, 'Album', 'BTS', 'album2.png', 'Jimin (BTS) \'FACE\' LP', 42.64, 'Dive into Jimin\'s heartfelt songs with the \'FACE\' LP, perfect for spinning on your turntable.'),
+(6, 'Album', 'BTS', 'album3.png', 'Jimin (BTS) \'MUSE\' (Set)', 32.47, 'Celebrate Jimin\'s artistry with this exclusive \'MUSE\' Set featuring stunning visuals and quotes.'),
+(7, 'Photobook', 'BTS', 'photobook1.png', 'Jin ‘Sea of JIN island’', 42.64, 'Discover the enchanting world of Jin with this beautifully designed product reflecting his personality.'),
+(8, 'Photobook', 'BTS', 'photobook2.png', 'BTS \'WE\' SET', 42.64, 'Celebrate unity with the \'WE\' SET, featuring items that embody the spirit of BTS.'),
+(9, 'Photobook', 'BTS', 'photobook3.png', 'SUGA ‘Wholly or Whole me’', 27.06, 'Explore SUGA\'s introspective journey with this unique product reflecting his depth and artistry.'),
+(10, 'Photocard', 'BTS', 'photocard1.png', 'BTS V layover POB PC', 7.00, 'Add to your collection with this stunning Proof of Purchase PC featuring V\'s charm.\r\n'),
+(11, 'Photocard', 'BTS', 'photocard2.jpg', 'Photocard Set (BTS)', 15.58, 'This exclusive Photocard Set includes high-quality images of BTS members, perfect for fans.'),
+(12, 'Photocard', 'BTS', 'photocard3.png', 'BTS V FRIENDS Photocards 2024', 20.68, 'Celebrate friendship with the BTS V FRIENDS Photocards 2024 set, capturing heartwarming moments.'),
+(13, 'Album', 'BTS', 'album_bts.png', 'RM (BTS) \'Right Place, Wrong Person\' (Set)', 48.70, 'Explore RM\'s themes with the \'Right Place, Wrong Person\' Set, inspired by his insightful lyrics.'),
+(15, 'Merchandise', 'BLACKPINK', 'merch4.png', 'OFFICIAL LIGHT STICK ver.2', 31.98, 'Illuminate your fandom with the OFFICIAL LIGHT STICK ver.2, perfect for concerts and events.'),
+(24, 'Photocard', 'BLACKPINK', 'photocard4.png', '[ME] JISOO PHOTO CARD DECO KIT', 9.84, 'Get creative with the [ME] JISOO PHOTO CARD DECO KIT, featuring beautiful stickers and accessories.');
 
 -- --------------------------------------------------------
 
@@ -403,7 +487,7 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`user_id`, `unique_id`, `fname`, `lname`, `gender`, `dob`, `country`, `country_code`, `handphone`, `email`, `address`, `postcode`, `city`, `about_me`, `password`, `img`, `background_img`, `status`) VALUES
 (1, 1655463620, 'Anna', 'Tanaka', 'Female', '2005-11-17', 'Japan', '+81', '0335467689', 'annajiang@gmail.com', 'Shibuya Ward, Jinnan 2-1-2-3 Shibuya Heights Room 101', '〒150-0041', 'Shibuya-ku, Tokyo-to', '                        Hi! Nice to meet you! I\\\'m Anna😄. Let\\\'s make friends 👥💖~', '$2y$10$vbSuA.lItGccFHB9s1lpjus45es7rOeNbl0lG1P.8rTC5.baNm4/y', 'anna.jpg', 'anna_background.png', 'Offline now'),
-(2, 1517014417, 'Ella', 'Gross', 'Female', NULL, NULL, NULL, NULL, 'ellagross@gmail.com', NULL, NULL, NULL, 'HELLO!', '$2y$10$xwIug7rPETLcgheGO9bqEuX32K/nz0imhlKtzwU6oh8QhMNPiB/W2', 'ella.jpg', 'ella_background.png', 'Offline now');
+(2, 1517014417, 'Ella', 'Gross', 'Female', '2008-11-25', 'United States', NULL, NULL, 'ellagross@gmail.com', NULL, NULL, NULL, '                        HELLO!                    ', '$2y$10$xwIug7rPETLcgheGO9bqEuX32K/nz0imhlKtzwU6oh8QhMNPiB/W2', 'ella.jpg', 'ella_background.png', 'Offline now');
 
 -- --------------------------------------------------------
 
@@ -437,6 +521,15 @@ INSERT INTO `wishlist` (`wishlist_id`, `unique_id`, `product_id`, `added_at`) VA
 --
 
 --
+-- Indexes for table `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`admin_id`),
+  ADD UNIQUE KEY `unique_id` (`unique_id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `admin_work_code` (`admin_work_code`);
+
+--
 -- Indexes for table `artists`
 --
 ALTER TABLE `artists`
@@ -450,6 +543,13 @@ ALTER TABLE `cart`
   ADD PRIMARY KEY (`cart_id`),
   ADD KEY `unique_id` (`unique_id`),
   ADD KEY `product_id` (`product_id`);
+
+--
+-- Indexes for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`feedback_id`),
+  ADD UNIQUE KEY `unique_id` (`unique_id`);
 
 --
 -- Indexes for table `friends`
@@ -480,6 +580,14 @@ ALTER TABLE `order_details`
   ADD PRIMARY KEY (`item_id`),
   ADD KEY `order_id` (`order_id`),
   ADD KEY `product_id` (`product_id`);
+
+--
+-- Indexes for table `order_issues`
+--
+ALTER TABLE `order_issues`
+  ADD PRIMARY KEY (`issue_id`),
+  ADD KEY `order_id` (`order_id`),
+  ADD KEY `unique_id` (`user_id`);
 
 --
 -- Indexes for table `personal_photos`
@@ -557,6 +665,12 @@ ALTER TABLE `wishlist`
 --
 
 --
+-- AUTO_INCREMENT for table `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
 -- AUTO_INCREMENT for table `artists`
 --
 ALTER TABLE `artists`
@@ -569,16 +683,22 @@ ALTER TABLE `cart`
   MODIFY `cart_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
 
 --
+-- AUTO_INCREMENT for table `feedback`
+--
+ALTER TABLE `feedback`
+  MODIFY `feedback_id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+
+--
 -- AUTO_INCREMENT for table `friends`
 --
 ALTER TABLE `friends`
-  MODIFY `friend_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `friend_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `msg_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
+  MODIFY `msg_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=103;
 
 --
 -- AUTO_INCREMENT for table `orders`
@@ -593,28 +713,34 @@ ALTER TABLE `order_details`
   MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
+-- AUTO_INCREMENT for table `order_issues`
+--
+ALTER TABLE `order_issues`
+  MODIFY `issue_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `personal_photos`
 --
 ALTER TABLE `personal_photos`
-  MODIFY `photo_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `photo_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `post_comments`
 --
 ALTER TABLE `post_comments`
-  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- AUTO_INCREMENT for table `post_likes`
 --
 ALTER TABLE `post_likes`
-  MODIFY `like_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `like_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- AUTO_INCREMENT for table `products`
@@ -626,7 +752,7 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT for table `rooms`
 --
 ALTER TABLE `rooms`
-  MODIFY `room_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `room_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `shipping_methods`
@@ -658,6 +784,12 @@ ALTER TABLE `cart`
   ADD CONSTRAINT `cart_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`);
 
 --
+-- Constraints for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD CONSTRAINT `fk_user` FOREIGN KEY (`unique_id`) REFERENCES `users` (`unique_id`) ON DELETE CASCADE;
+
+--
 -- Constraints for table `friends`
 --
 ALTER TABLE `friends`
@@ -677,6 +809,13 @@ ALTER TABLE `orders`
 ALTER TABLE `order_details`
   ADD CONSTRAINT `order_details_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`),
   ADD CONSTRAINT `order_details_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`);
+
+--
+-- Constraints for table `order_issues`
+--
+ALTER TABLE `order_issues`
+  ADD CONSTRAINT `order_issues_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `order_issues_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`unique_id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `personal_photos`
@@ -728,5 +867,3 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-
